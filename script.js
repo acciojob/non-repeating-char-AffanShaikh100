beb4e6041016e9a4f6d8039ffe30486d56ef5f06
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-13 21:00:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	
 	for(let i=0; i<str.length;i++)
 		{
-			let count = 0;
-			for(let j=i+1;j<str.length;j++)
+			let count=0;
+		    for(let j=i+1; j<str.length; j++)
 				{
-					if(str[i]==str[j] ){
-						count++;
+					if(str[i]==str[j])
+					{
+						count+=1;
 					}
 				}
-			if(count = 0){
-				return str[i];
-			}
-		} 
-	return null;
+			if(count==0){  
+				return[str[i];
+			} 
+		}     
+	return null; 
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
